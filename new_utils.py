1799926dd5cb41bd8063e2ac166becedcb8dfa6f
Nishad Nakhwa <55823970+NishadN08@users.noreply.github.com>
2024-02-22 21:55:39 -0500

    Add files via upload

--- new_utils.py
+++ new_utils.py
@@ -1,13 +1,14 @@
+
 """
    Use to create your own functions for reuse 
    across the assignment
 
    Inside part_1_template_solution.py, 
-  
+
      import new_utils
-  
+
     or
-  
+
      import new_utils as nu
 """
 import numpy as np
